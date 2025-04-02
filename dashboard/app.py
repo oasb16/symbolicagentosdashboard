4e55785b05ad30bb4bbe30ca5f4023d7d1d25280
Omkar Sawant Bhosle <omkarsbdev@gmail.com>
2025-04-02 12:42:39 -0700

    Init Commit

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from dashboard.agenda_tracker import generate_agenda_ui
+from agenda_tracker import generate_agenda_ui
 
 st.set_page_config(page_title="AGENDΔ_CORE Dashboard", layout="wide")
 generate_agenda_ui()
